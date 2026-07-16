ac180762635ed7aa806d6d815828b58dc2f5a540
unborncompany <124112426+unborncompany@users.noreply.github.com>
2026-07-16 15:29:35 -0400

    Update cache name to gol-cache-v2.7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ============ sw.js — Service Worker ============ */
-const CACHE_NAME = "gol-cache-v2.6";
+const CACHE_NAME = "gol-cache-v2.7";
 const DEFAULT_INVENTORY_URL = "/default-inventory.json";
 
 const SHELL_URLS = [
